dd60b482bba3f4cc9368bfe906840d45f6fc1928
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2026-04-19 18:19:04 +0400

    fix: improve feedback handling and formatting in PostFeedback component

--- src/components/common/PostFeedback.svelte
+++ src/components/common/PostFeedback.svelte
@@ -61,7 +61,9 @@
       notHelpful = updatedFeedback.notHelpful || 0;
 
       // Save feedback locally to prevent multiple submissions for this slug
-      const savedFeedback = JSON.parse(localStorage.getItem('feedback') || '{}');
+      const savedFeedback = JSON.parse(
+        localStorage.getItem('feedback') || '{}'
+      );
       savedFeedback[slug] = type;
       localStorage.setItem('feedback', JSON.stringify(savedFeedback));
 
@@ -73,31 +75,33 @@
   }
 </script>
 
-<div class="mt-12 flex flex-col sm:flex-row gap-y-2 items-center justify-center gap-x-2 sm:gap-y-0">
+<div
+  class="mt-12 flex flex-col sm:flex-row gap-y-2 items-center justify-center gap-x-2 sm:gap-y-0"
+>
   <slot />
   <h3 class="text-slate-700">{title}</h3>
   <div>
     <button
-    type="button"
-    onclick={() => handleFeedback("helpful")}
-    disabled={feedbackGiven}
-    class="group inline-flex items-center gap-x-2 rounded-lg border border-slate-400 px-3 py-2 text-sm font-medium text-slate-700 hover:border-teal-500 hover:bg-teal-500"
-    class:bg-teal-500={userChoice === "helpful"}
-    class:border-teal-500={userChoice === "helpful"}
-  >
-    <slot name="helpfulIcon" /> {firstChoice}</button
-  >
+      type="button"
+      onclick={() => handleFeedback('helpful')}
+      disabled={feedbackGiven}
+      class="group inline-flex items-center gap-x-2 rounded-lg border border-slate-400 px-3 py-2 text-sm font-medium text-slate-700 hover:border-teal-500 hover:bg-teal-500"
+      class:bg-teal-500={userChoice === 'helpful'}
+      class:border-teal-500={userChoice === 'helpful'}
+    >
+      <slot name="helpfulIcon" /> {firstChoice}</button
+    >
     <button
-    type="button"
-    onclick={() => handleFeedback("notHelpful")}
-    disabled={feedbackGiven}
-    class="group inline-flex items-center gap-x-2 rounded-lg border border-slate-400 px-3 py-2 text-sm font-medium text-slate-700 hover:bg-slate-300"
-    class:bg-slate-300={userChoice === "notHelpful"}
-  >
-    <slot name="notHelpfulIcon" />
-    {secondChoice}
-  </button>
-</div>
+      type="button"
+      onclick={() => handleFeedback('notHelpful')}
+      disabled={feedbackGiven}
+      class="group inline-flex items-center gap-x-2 rounded-lg border border-slate-400 px-3 py-2 text-sm font-medium text-slate-700 hover:bg-slate-300"
+      class:bg-slate-300={userChoice === 'notHelpful'}
+    >
+      <slot name="notHelpfulIcon" />
+      {secondChoice}
+    </button>
+  </div>
 </div>
 
 {#if helpful > 0}
